bf83ba45643f2d355cb985b4033ba301759752ee
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-02-15 16:18:48 +0530

    Remove toast notification from AddToCartButton
    Removed toast notification after adding product to cart.

--- src/components/add-to-cart-button.tsx
+++ src/components/add-to-cart-button.tsx
@@ -3,7 +3,6 @@
 import { Product } from "@/lib/definitions";
 import { useCart } from "@/providers/cart-provider";
 import { Button } from "@/components/ui/button";
-import { useToast } from "@/components/ui/use-toast";
 
 interface Props {
   product: Product;
@@ -11,15 +10,9 @@ interface Props {
 
 export default function AddToCartButton({ product }: Props) {
   const { addToCart } = useCart();
-  const { toast } = useToast();
 
   function handleAdd() {
     addToCart(product);
-
-    toast({
-      title: "Added to Cart 🛒",
-      description: `${product.name} has been added successfully.`,
-    });
   }
 
   return (
