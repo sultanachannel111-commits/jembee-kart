43bc7be593a82e35552a12d0f41f283613cc75bc
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-02-16 19:22:25 +0530

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,42 @@
 export default function DashboardPage() {
   return (
-    <div className="max-w-7xl mx-auto p-6 min-h-screen">
-      <h1 className="text-3xl font-bold mb-6">Dashboard</h1>
-      <p>Welcome to Admin Dashboard.</p>
+    <div className="min-h-screen bg-gray-100 p-10">
+
+      <h1 className="text-3xl font-bold mb-8">
+        Admin Dashboard
+      </h1>
+
+      <div className="grid md:grid-cols-3 gap-6">
+
+        <div className="bg-white p-6 rounded-xl shadow-md">
+          <h2 className="text-xl font-semibold mb-2">
+            Total Orders
+          </h2>
+          <p className="text-3xl font-bold text-green-600">
+            0
+          </p>
+        </div>
+
+        <div className="bg-white p-6 rounded-xl shadow-md">
+          <h2 className="text-xl font-semibold mb-2">
+            Total Products
+          </h2>
+          <p className="text-3xl font-bold text-blue-600">
+            6
+          </p>
+        </div>
+
+        <div className="bg-white p-6 rounded-xl shadow-md">
+          <h2 className="text-xl font-semibold mb-2">
+            Revenue
+          </h2>
+          <p className="text-3xl font-bold text-purple-600">
+            ₹0
+          </p>
+        </div>
+
+      </div>
+
     </div>
   );
 }
