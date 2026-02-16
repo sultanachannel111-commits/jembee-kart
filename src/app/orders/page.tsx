e6411741551fba8eb96e9e08f818a9e9d67ec1a2
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-02-16 18:44:35 +0530

    Update page.tsx

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Header from "@/components/header";
 
 export default function OrdersPage() {
   const [orders, setOrders] = useState<any[]>([]);
