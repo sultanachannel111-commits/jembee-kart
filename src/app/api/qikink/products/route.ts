2f6a9bd551a18d2432dace48cf907772194aaa0e
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-02-19 13:25:26 +0530

    Update route.ts

--- src/app/api/qikink/products/route.ts
+++ src/app/api/qikink/products/route.ts
@@ -2,30 +2,34 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const clientId = process.env.QIKINK_CLIENT_ID!;
-    const clientSecret = process.env.QIKINK_CLIENT_SECRET!;
-
-    const auth = Buffer.from(`${clientId}:${clientSecret}`).toString("base64");
 
     const response = await fetch(
       "https://sandbox.qikink.com/api/v1/catalog/products",
       {
         method: "GET",
         headers: {
-          Authorization: `Basic ${auth}`,
           "Content-Type": "application/json",
+          "ClientId": process.env.QIKINK_CLIENT_ID!,
+          "AccessToken": process.env.QIKINK_ACCESS_TOKEN!
         },
       }
     );
 
     const data = await response.json();
 
-    if (!data.products || data.products.length === 0) {
-      return NextResponse.json({ message: "No products found" });
+    if (!response.ok) {
+      return NextResponse.json(
+        { error: data },
+        { status: response.status }
+      );
     }
 
     return NextResponse.json(data);
+
   } catch (error) {
-    return NextResponse.json({ error: "Import Failed" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Server Error" },
+      { status: 500 }
+    );
   }
 }
