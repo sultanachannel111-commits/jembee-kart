c7d0384f3b7cdee6eb0e43acc1a572bfd7bb60b5
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-02-20 19:35:55 +0530

    Update route.ts

--- src/app/api/qikink/create-order/route.ts
+++ src/app/api/qikink/create-order/route.ts
@@ -5,7 +5,8 @@ export async function GET() {
     const clientId = process.env.QIKINK_CLIENT_ID!;
     const clientSecret = process.env.QIKINK_CLIENT_SECRET!;
 
-    const response = await fetch(
+    // STEP 1: Get Token
+    const tokenResponse = await fetch(
       "https://sandbox.qikink.com/api/token",
       {
         method: "POST",
@@ -19,12 +20,48 @@ export async function GET() {
       }
     );
 
-    const data = await response.json();
+    const tokenData = await tokenResponse.json();
+    const accessToken = tokenData.Accesstoken;
 
-    return NextResponse.json(data);
+    // STEP 2: Create Order
+    const orderResponse = await fetch(
+      "https://sandbox.qikink.com/api/order/create",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          order_id: "TEST12345",
+          shipping_address: {
+            name: "Ali Test",
+            address1: "Test Street 123",
+            city: "Jamshedpur",
+            state: "Jharkhand",
+            pincode: "832110",
+            country: "India",
+            phone: "9999999999",
+          },
+          order_items: [
+            {
+              product_id: "63784036",
+              quantity: 1,
+            },
+          ],
+        }),
+      }
+    );
+
+    const orderData = await orderResponse.json();
+
+    return NextResponse.json(orderData, {
+      status: orderResponse.status,
+    });
+
   } catch (error) {
     return NextResponse.json(
-      { error: "Token request failed ❌" },
+      { error: "Order failed ❌" },
       { status: 500 }
     );
   }
