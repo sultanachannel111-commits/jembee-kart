bca41da14268166bacfc6c9f29b377b1fccb1e72
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-02-21 23:46:15 +0530

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,24 +1,60 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/lib/firebase";
+
 export default function DashboardPage() {
+  const [orders, setOrders] = useState(0);
+  const [products, setProducts] = useState(0);
+  const [users, setUsers] = useState(0);
+  const [revenue, setRevenue] = useState(0);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const orderSnap = await getDocs(collection(db, "orders"));
+    const productSnap = await getDocs(collection(db, "products"));
+    const userSnap = await getDocs(collection(db, "users"));
+
+    let totalRevenue = 0;
+
+    orderSnap.forEach((doc) => {
+      const data = doc.data();
+      totalRevenue += data.price || 0;
+    });
+
+    setOrders(orderSnap.size);
+    setProducts(productSnap.size);
+    setUsers(userSnap.size);
+    setRevenue(totalRevenue);
+  };
+
   return (
     <div>
-      <h1 className="text-3xl font-bold mb-6">Admin Dashboard</h1>
+      <h1 className="text-3xl font-bold mb-8">
+        Dashboard Overview 📊
+      </h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <div className="bg-white p-6 rounded-xl shadow">
-          <h2 className="text-lg font-semibold">Total Products</h2>
-          <p className="text-2xl mt-2">12</p>
-        </div>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
 
-        <div className="bg-white p-6 rounded-xl shadow">
-          <h2 className="text-lg font-semibold">Total Orders</h2>
-          <p className="text-2xl mt-2">5</p>
-        </div>
+        <Card title="Total Orders" value={orders} />
+        <Card title="Total Revenue" value={`₹${revenue}`} />
+        <Card title="Total Products" value={products} />
+        <Card title="Total Users" value={users} />
 
-        <div className="bg-white p-6 rounded-xl shadow">
-          <h2 className="text-lg font-semibold">Revenue</h2>
-          <p className="text-2xl mt-2">₹15,000</p>
-        </div>
       </div>
+    </div>
+  );
+}
+
+function Card({ title, value }: any) {
+  return (
+    <div className="bg-white p-6 rounded-xl shadow">
+      <h3 className="text-gray-500 text-sm">{title}</h3>
+      <p className="text-2xl font-bold">{value}</p>
     </div>
   );
 }
