a8beb8f428fae3e80b3adf8ea9d000efde0e0bf2
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-03 22:51:50 +0530

    Create AuthContext.tsx

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "@/lib/firebase";
+import { onAuthStateChanged, User } from "firebase/auth";
+
+type AuthType = {
+  user: User | null;
+};
+
+const AuthContext = createContext<AuthType>({ user: null });
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuth = () => useContext(AuthContext);
