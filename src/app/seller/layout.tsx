7f33a471fd43eee9b07ae5537be6f28db31d793a
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-04 22:49:52 +0530

    Update layout.tsx

--- src/app/seller/layout.tsx
+++ src/app/seller/layout.tsx
@@ -3,57 +3,34 @@
 import Link from "next/link";
 
 export default function SellerLayout({
-children,
-}:{
-children:React.ReactNode
-}){
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen">
 
-return(
+      {/* Sidebar */}
+      <div className="w-64 bg-black text-white p-6 space-y-4">
 
-<div className="flex min-h-screen">
+        <h2 className="text-xl font-bold text-pink-500">
+          Seller Panel
+        </h2>
 
-{/* Sidebar */}
+        <Link href="/seller">Dashboard</Link>
+        <Link href="/seller/add-product">Add Product</Link>
+        <Link href="/seller/products">Products</Link>
+        <Link href="/seller/orders">Orders</Link>
+        <Link href="/seller/revenue">Revenue</Link>
+        <Link href="/seller/account">Account</Link>
 
-<div className="w-64 bg-black text-white p-6 space-y-4">
+      </div>
 
-<h2 className="text-xl font-bold text-pink-500">
-Seller Panel
-</h2>
+      {/* Content */}
+      <div className="flex-1 p-6 bg-gray-100">
+        {children}
+      </div>
 
-<Link href="/seller">Dashboard</Link>
-
-<Link href="/seller/add-product">
-Add Product
-</Link>
-
-<Link href="/seller/products">
-Products
-</Link>
-
-<Link href="/seller/orders">
-Orders
-</Link>
-
-<Link href="/seller/revenue">
-Revenue
-</Link>
-
-<Link href="/seller/account">
-Account
-</Link>
-
-</div>
-
-{/* Content */}
-
-<div className="flex-1 p-6 bg-gray-100">
-
-{children}
-
-</div>
-
-</div>
-
-)
-
+    </div>
+  );
 }
