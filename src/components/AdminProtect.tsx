18c17e0b9ba676ff009ec7c5c1f3b68b69d80440
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-04 10:10:28 +0530

    Update AdminProtect.tsx

--- src/components/AdminProtect.tsx
+++ src/components/AdminProtect.tsx
@@ -24,7 +24,7 @@ export default function AdminProtect({
       const userDoc = await getDoc(doc(db, "users", user.uid));
 
       if (!userDoc.exists()) {
-  router.push("/")
+  router.push("/");
   return;
 }
 
