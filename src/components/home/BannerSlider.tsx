3be84e7986162a6a0e8835e76ca57f601c782704
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-06 18:46:14 +0530

    Update BannerSlider.tsx

--- src/components/home/BannerSlider.tsx
+++ src/components/home/BannerSlider.tsx
@@ -1,1 +1,23 @@
-i
+"use client";
+
+export default function BannerSlider({
+banners,
+slide
+}: any){
+
+if(!banners.length) return null;
+
+return(
+
+<div className="mt-2">
+
+<img
+src={banners[slide]?.image}
+className="w-full h-[160px] object-cover rounded-xl"
+/>
+
+</div>
+
+);
+
+}
