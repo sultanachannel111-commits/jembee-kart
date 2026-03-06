31c52901ce7fb513677d565e327d89f00a7f6de8
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-06 18:49:41 +0530

    Update BottomNav.tsx

--- src/components/home/BottomNav.tsx
+++ src/components/home/BottomNav.tsx
@@ -1,1 +1,36 @@
-BottomNav.tsx
+"use client";
+
+import Link from "next/link";
+import { Home, Grid, Flame, User } from "lucide-react";
+
+export default function BottomNav(){
+
+return(
+
+<div className="fixed bottom-0 left-0 right-0 bg-white border-t flex justify-around py-2">
+
+<Link href="/" className="flex flex-col items-center text-xs">
+<Home size={20}/>
+Home
+</Link>
+
+<Link href="/categories" className="flex flex-col items-center text-xs">
+<Grid size={20}/>
+Categories
+</Link>
+
+<Link href="/offers" className="flex flex-col items-center text-xs">
+<Flame size={20}/>
+Offers
+</Link>
+
+<Link href="/profile" className="flex flex-col items-center text-xs">
+<User size={20}/>
+Profile
+</Link>
+
+</div>
+
+);
+
+}
