a8b82cca80df47663c8d4693323a94c07dde4339
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-06 13:42:32 +0530

    Create aiHomepageEngine.ts

---
+++ src/services/aiHomepageEngine.ts
@@ -0,0 +1,41 @@
+import { getDocs,collection } from "firebase/firestore";
+import { db } from "@/lib/firebase";
+
+export const generateAutoLayout = async ()=>{
+
+const offers = await getDocs(collection(db,"offers"));
+const festival = await getDocs(collection(db,"festivalBanner"));
+const products = await getDocs(collection(db,"products"));
+
+let layout=[];
+
+
+/* FESTIVAL PRIORITY */
+
+if(!festival.empty){
+
+layout.push("festival");
+
+}
+
+
+/* OFFERS PRIORITY */
+
+if(offers.size>0){
+
+layout.push("offers");
+
+}
+
+
+/* DEFAULT */
+
+layout.push("banner");
+layout.push("categories");
+layout.push("trending");
+layout.push("products");
+
+
+return layout;
+
+};
