39ea50aced2727ce57e598b37740fc315a5e157a
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-06 18:47:01 +0530

    Update FestivalBanner.tsx

--- src/components/home/FestivalBanner.tsx
+++ src/components/home/FestivalBanner.tsx
@@ -1,1 +1,30 @@
-i
+"use client";
+
+export default function FestivalBanner({
+festival,
+timeLeft
+}: any){
+
+if(!festival || !timeLeft) return null;
+
+return(
+
+<div className="bg-gradient-to-r from-pink-500 to-purple-600 text-white rounded-xl p-4">
+
+<h2 className="font-bold text-lg">
+{festival.title}
+</h2>
+
+<div className="flex gap-4 mt-2 font-bold">
+
+<span>{timeLeft.hours}h</span>
+<span>{timeLeft.minutes}m</span>
+<span>{timeLeft.seconds}s</span>
+
+</div>
+
+</div>
+
+);
+
+}
