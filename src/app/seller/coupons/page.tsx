0510bdc3bfc53d032971aa170116159b843bea11
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-08 07:58:36 +0530

    Create page.tsx

---
+++ src/app/seller/coupons/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+export default function Coupons(){
+
+return(
+
+<div>
+
+<h1 className="text-2xl font-bold mb-6">
+Coupons
+</h1>
+
+<p className="text-gray-500">
+Create discount coupons here
+</p>
+
+</div>
+
+)
+
+}
