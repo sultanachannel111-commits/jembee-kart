476878b1d6575a700e9a8eb4dcdbef3ad8901bda
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-08 08:16:13 +0530

    Update page.tsx

--- src/app/seller/messages/page.tsx
+++ src/app/seller/messages/page.tsx
@@ -1,1 +1,25 @@
-src/app/seller/messages/page.tsx
+"use client";
+
+export default function Messages() {
+
+return (
+
+<div className="p-6">
+
+<h1 className="text-2xl font-bold mb-6">
+Customer Messages
+</h1>
+
+<div className="bg-white shadow rounded-xl p-6">
+
+<p className="text-gray-500">
+No messages yet
+</p>
+
+</div>
+
+</div>
+
+);
+
+}
