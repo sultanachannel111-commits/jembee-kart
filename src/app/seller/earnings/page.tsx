beb44786b982cb4fbc1c057fe25f565edaddeb86
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-08 07:49:34 +0530

    Create page.tsx

---
+++ src/app/seller/earnings/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+export default function Earnings(){
+
+return(
+
+<div>
+
+<h1 className="text-2xl font-bold">
+Earnings
+</h1>
+
+<p className="text-xl mt-4">
+₹0
+</p>
+
+</div>
+
+)
+
+}
