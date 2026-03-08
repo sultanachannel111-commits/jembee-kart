94992f4fc509fe4926fa8d8e9040c6cb7344c3f6
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-08 07:56:13 +0530

    Create page.tsx

---
+++ src/app/seller/reviews/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+export default function Reviews(){
+
+return(
+
+<div>
+
+<h1 className="text-2xl font-bold mb-6">
+Customer Reviews
+</h1>
+
+<p className="text-gray-500">
+No reviews yet
+</p>
+
+</div>
+
+)
+
+}
