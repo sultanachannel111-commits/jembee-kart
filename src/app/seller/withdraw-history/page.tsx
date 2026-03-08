512e655cf2d1844d015f83202bf2500aa0fbe402
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-08 07:55:30 +0530

    Create page.tsx

---
+++ src/app/seller/withdraw-history/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+export default function WithdrawHistory(){
+
+return(
+
+<div>
+
+<h1 className="text-2xl font-bold mb-6">
+Withdraw History
+</h1>
+
+<p className="text-gray-500">
+No withdraw history yet
+</p>
+
+</div>
+
+)
+
+}
