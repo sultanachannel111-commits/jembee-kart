8fb97a6284db2c281f63eb074dde1eb504166fde
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-08 07:53:59 +0530

    Create page.tsx

---
+++ src/app/seller/inventory/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+export default function Inventory(){
+
+return(
+
+<div>
+
+<h1 className="text-2xl font-bold mb-6">
+Inventory
+</h1>
+
+<div className="bg-white shadow rounded-xl p-4">
+
+<p className="text-gray-500">
+Stock management system coming soon
+</p>
+
+</div>
+
+</div>
+
+)
+
+}
