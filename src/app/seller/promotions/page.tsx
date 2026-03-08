356b5fe44aa0e8afdb6006f85009ccc9606ebc74
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-08 07:57:39 +0530

    Create page.tsx

---
+++ src/app/seller/promotions/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+export default function Promotions(){
+
+return(
+
+<div>
+
+<h1 className="text-2xl font-bold mb-6">
+Product Promotions
+</h1>
+
+<p className="text-gray-500">
+Promotion tools coming soon
+</p>
+
+</div>
+
+)
+
+}
