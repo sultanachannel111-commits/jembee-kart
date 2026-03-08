73207ffe7aa114feeae3c5b1ade1d26f543a42f5
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-08 07:44:38 +0530

    Update page.tsx

--- src/app/seller/add-product/page.tsx
+++ src/app/seller/add-product/page.tsx
@@ -4,12 +4,13 @@ import { useState } from "react";
 
 export default function AddProduct(){
 
-const [name,setName] = useState("");
-const [price,setPrice] = useState("");
+const [name,setName] = useState("")
+const [price,setPrice] = useState("")
+const [desc,setDesc] = useState("")
 
 return(
 
-<div className="p-6">
+<div className="max-w-xl">
 
 <h1 className="text-2xl font-bold mb-6">
 Add Product
@@ -19,22 +20,31 @@ Add Product
 placeholder="Product Name"
 value={name}
 onChange={(e)=>setName(e.target.value)}
-className="border p-2 w-full mb-3"
+className="border p-3 w-full mb-3 rounded"
+/>
+
+<textarea
+placeholder="Description"
+value={desc}
+onChange={(e)=>setDesc(e.target.value)}
+className="border p-3 w-full mb-3 rounded"
 />
 
 <input
 placeholder="Price"
 value={price}
 onChange={(e)=>setPrice(e.target.value)}
-className="border p-2 w-full mb-3"
+className="border p-3 w-full mb-3 rounded"
 />
 
-<button className="bg-black text-white px-4 py-2 rounded">
+<button
+className="bg-black text-white px-5 py-3 rounded"
+>
 Publish Product
 </button>
 
 </div>
 
-);
+)
 
 }
