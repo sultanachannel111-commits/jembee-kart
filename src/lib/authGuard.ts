d1fc3e69064909c2f0d1c7773722f5855bd1f935
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-09 05:31:45 +0530

    Create authGuard.ts

---
+++ src/lib/authGuard.ts
@@ -0,0 +1,16 @@
+import { auth, db } from "@/lib/firebase";
+import { doc, getDoc } from "firebase/firestore";
+
+export async function getUserRole(){
+
+const user = auth.currentUser;
+
+if(!user) return null;
+
+const snap = await getDoc(doc(db,"users",user.uid));
+
+if(!snap.exists()) return null;
+
+return snap.data();
+
+}
