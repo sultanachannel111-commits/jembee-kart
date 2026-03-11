477b1ffe994ff13e061082cee6b4cce37b0c6b7e
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-11 13:09:36 +0530

    Update page.tsx

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import { useState } from "react";
+import { useSearchParams } from "next/navigation";
 
 export default function CheckoutPage() {
+
+const searchParams = useSearchParams();
+const price = Number(searchParams.get("price")) || 0;
 
 const [loading,setLoading] = useState(false);
 
@@ -22,42 +26,13 @@ INDIA STATES
 ========================= */
 
 const states = [
-"Andhra Pradesh",
-"Arunachal Pradesh",
-"Assam",
-"Bihar",
-"Chhattisgarh",
-"Goa",
-"Gujarat",
-"Haryana",
-"Himachal Pradesh",
-"Jharkhand",
-"Karnataka",
-"Kerala",
-"Madhya Pradesh",
-"Maharashtra",
-"Manipur",
-"Meghalaya",
-"Mizoram",
-"Nagaland",
-"Odisha",
-"Punjab",
-"Rajasthan",
-"Sikkim",
-"Tamil Nadu",
-"Telangana",
-"Tripura",
-"Uttar Pradesh",
-"Uttarakhand",
-"West Bengal",
-"Andaman and Nicobar Islands",
-"Chandigarh",
-"Dadra and Nagar Haveli and Daman and Diu",
-"Delhi",
-"Jammu and Kashmir",
-"Ladakh",
-"Lakshadweep",
-"Puducherry"
+"Andhra Pradesh","Arunachal Pradesh","Assam","Bihar","Chhattisgarh","Goa",
+"Gujarat","Haryana","Himachal Pradesh","Jharkhand","Karnataka","Kerala",
+"Madhya Pradesh","Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland",
+"Odisha","Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura",
+"Uttar Pradesh","Uttarakhand","West Bengal","Andaman and Nicobar Islands",
+"Chandigarh","Dadra and Nagar Haveli and Daman and Diu","Delhi",
+"Jammu and Kashmir","Ladakh","Lakshadweep","Puducherry"
 ];
 
 /* =========================
@@ -106,7 +81,7 @@ const product = {
 name:"Custom T-shirt",
 sku:"TSHIRT001",
 printTypeId:"1",
-sellingPrice:499,
+sellingPrice:price,
 designLink:"https://example.com/design.png",
 mockupLink:"https://example.com/mockup.png"
 };
@@ -186,15 +161,11 @@ setCustomer({...customer,address:e.target.value})
 }
 />
 
-{/* PINCODE */}
-
 <input
 placeholder="Pin Code"
 className="border p-3 w-full rounded"
 onChange={(e)=>handlePincode(e.target.value)}
 />
-
-{/* CITY */}
 
 <input
 placeholder="City"
@@ -204,8 +175,6 @@ onChange={(e)=>
 setCustomer({...customer,city:e.target.value})
 }
 />
-
-{/* STATE AUTOCOMPLETE */}
 
 <input
 list="states"
@@ -255,4 +224,3 @@ className="bg-pink-500 text-white px-6 py-3 rounded w-full"
 );
 
 }
-
