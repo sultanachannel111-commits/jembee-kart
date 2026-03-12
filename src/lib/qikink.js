2328e4f218cc12dd3cea76a03e3677db3f12f5f8
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-12 21:34:09 +0530

    Update qikink.js

--- src/lib/qikink.js
+++ src/lib/qikink.js
@@ -1,11 +1,24 @@
 export async function getQikinkProducts() {
-  const res = await fetch("https://api.qikink.com/api/v1/products", {
+
+  const tokenRes = await fetch("https://api.qikink.com/oauth/token", {
+    method: "POST",
     headers: {
-      Authorization: "Bearer 4216a1ee1ef57511ef9bf2d6c4cd83689a84e4a9881d50b301c347f42354dcc7",
-      "Content-Type": "application/json",
+      "Content-Type": "application/json"
     },
+    body: JSON.stringify({
+      client_id: "827265200202480",
+      client_secret: "4216a1ee1ef57511ef9bf2d6c4cd83689a84e4a9881d50b301c347f42354dcc7",
+      grant_type: "client_credentials"
+    })
   });
 
-  const data = await res.json();
-  return data;
+  const tokenData = await tokenRes.json();
+
+  const res = await fetch("https://api.qikink.com/api/v1/products", {
+    headers: {
+      Authorization: `Bearer ${tokenData.access_token}`
+    }
+  });
+
+  return await res.json();
 }
