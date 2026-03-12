1055efad40e6f483d88fe35a27d3641dab8cd199
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-12 22:02:24 +0530

    Update qikink.js

--- src/lib/qikink.js
+++ src/lib/qikink.js
@@ -1,24 +1,33 @@
 export async function getQikinkProducts() {
 
-  const tokenRes = await fetch("https://api.qikink.com/oauth/token", {
+  const CLIENT_ID = "827265200202480";
+  const CLIENT_SECRET = "4216a1ee1ef57511ef9bf2d6c4cd83689a84e4a9881d50b301c347f42354dcc7";
+
+  const tokenRes = await fetch("https://api.qikink.com/api/v1/oauth/token", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
-      client_id: "827265200202480",
-      client_secret: "4216a1ee1ef57511ef9bf2d6c4cd83689a84e4a9881d50b301c347f42354dcc7",
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
       grant_type: "client_credentials"
     })
   });
 
   const tokenData = await tokenRes.json();
+  console.log("TOKEN:", tokenData);
 
-  const res = await fetch("https://api.qikink.com/api/v1/products", {
+  const accessToken = tokenData.access_token;
+
+  const res = await fetch("https://api.qikink.com/api/v1/catalog/products", {
     headers: {
-      Authorization: `Bearer ${tokenData.access_token}`
+      Authorization: `Bearer ${accessToken}`
     }
   });
 
-  return await res.json();
+  const data = await res.json();
+  console.log("QIKINK PRODUCTS:", data);
+
+  return data.products || [];
 }
