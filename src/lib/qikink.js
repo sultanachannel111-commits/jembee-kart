de1f467fae26bfcd43be4dd15064ef71bb436861
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-13 08:02:24 +0530

    Update qikink.js

--- src/lib/qikink.js
+++ src/lib/qikink.js
@@ -1,56 +1,34 @@
 export async function getQikinkProducts() {
 
-const CLIENT_ID = "827265200202480";
-const CLIENT_SECRET = "4216a1ee1ef57511ef9bf2d6c4cd83689a84e4a9881d50b301c347f42354dcc7";
+  const CLIENT_ID = "827265200202480";
+  const CLIENT_SECRET = "4216a1ee1ef57511ef9bf2d6c4cd83689a84e4a9881d50b301c347f42354dcc7";
 
-try {
+  const tokenRes = await fetch("https://api.qikink.com/api/v1/oauth/token",{
+    method:"POST",
+    headers:{ "Content-Type":"application/json"},
+    body:JSON.stringify({
+      client_id:CLIENT_ID,
+      client_secret:CLIENT_SECRET,
+      grant_type:"client_credentials"
+    })
+  });
 
-const tokenRes = await fetch(
-"https://api.qikink.com/api/v1/oauth/token",
-{
-method:"POST",
-headers:{
-"Content-Type":"application/json"
-},
-body:JSON.stringify({
-client_id:CLIENT_ID,
-client_secret:CLIENT_SECRET,
-grant_type:"client_credentials"
-})
-}
-);
+  const tokenData = await tokenRes.json();
 
-const tokenData = await tokenRes.json();
+  console.log("TOKEN DATA:",tokenData);
 
-console.log("TOKEN:",tokenData);
+  const accessToken = tokenData.access_token;
 
-const accessToken = tokenData.access_token;
+  const res = await fetch("https://api.qikink.com/api/v1/catalog/products",{
+    headers:{
+      Authorization:`Bearer ${accessToken}`
+    }
+  });
 
-if(!accessToken){
-console.log("Token not received");
-return [];
-}
+  const data = await res.json();
 
-const res = await fetch(
-"https://api.qikink.com/api/v1/catalog/products",
-{
-headers:{
-Authorization:`Bearer ${accessToken}`
-}
-}
-);
+  console.log("PRODUCT API DATA:",data);
 
-const data = await res.json();
-
-console.log("QIKINK PRODUCTS:",data);
-
-return data?.data?.products || [];
-
-}catch(err){
-
-console.log("Qikink Error:",err);
-return [];
-
-}
+  return data.products || [];
 
 }
