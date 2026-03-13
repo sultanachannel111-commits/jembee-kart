9ec8874c609319b6ec0948a621bb5720926f0f74
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-13 10:42:17 +0530

    Update route.ts

--- src/app/api/qikink/route.ts
+++ src/app/api/qikink/route.ts
@@ -3,11 +3,32 @@ import { db } from "@/lib/firebase";
 import { collection, addDoc } from "firebase/firestore";
 
 export async function GET() {
+
   try {
 
+    // STEP 1: TOKEN GENERATE
+    const tokenRes = await fetch("https://api.qikink.com/api/v1/oauth/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        client_id: "827265200202480",
+        client_secret: "4216a1ee1ef57511ef9bf2d6c4cd83689a84e4a9881d50b301c347f42354dcc7",
+        grant_type: "client_credentials"
+      })
+    });
+
+    const tokenData = await tokenRes.json();
+
+    const accessToken = tokenData.access_token;
+
+
+    // STEP 2: GET DESIGNS
     const res = await fetch("https://api.qikink.com/api/v1/catalog/designs", {
       method: "GET",
       headers: {
+        "Authorization": `Bearer ${accessToken}`,
         "Content-Type": "application/json"
       }
     });
@@ -21,6 +42,7 @@ export async function GET() {
     for (const d of designs) {
 
       await addDoc(collection(db, "products"), {
+
         qikink_id: d.id,
 
         name: d.title || "Qikink Product",
@@ -32,9 +54,11 @@ export async function GET() {
           "https://via.placeholder.com/300",
 
         supplier: "qikink"
+
       });
 
       count++;
+
     }
 
     return NextResponse.json({
@@ -45,9 +69,10 @@ export async function GET() {
   } catch (error) {
 
     return NextResponse.json({
-      error: "Import failed",
-      details: error
+      error: "Import Failed",
+      details: String(error)
     });
 
   }
+
 }
