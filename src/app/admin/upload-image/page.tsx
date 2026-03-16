d9bc0086f2fb7d009863eb18d5b5e3e74b178e7c
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-17 05:05:04 +0530

    Update page.tsx

--- src/app/admin/upload-image/page.tsx
+++ src/app/admin/upload-image/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 
-export default function UploadImage(){
+export default function UploadImage() {
 
 const [file,setFile] = useState<File | null>(null);
 const [preview,setPreview] = useState("");
@@ -18,7 +18,7 @@ setPreview(URL.createObjectURL(selected));
 
 }
 
-async function upload(){
+function upload(){
 
 if(!file) return alert("Select image first");
 
@@ -28,16 +28,16 @@ reader.onload = () => {
 
 const base64 = reader.result as string;
 
-// image localStorage में save
+// image save
 localStorage.setItem("uploadedImage", base64);
 
-alert("Image Uploaded");
-
-// product page पर वापस
-const type = new URLSearchParams(window.location.search).get("type") || "main";
+// type save
+const type =
+new URLSearchParams(window.location.search).get("type") || "main";
 
 localStorage.setItem("uploadedImageType", type);
 
+// redirect
 window.location.href = "/admin/qikink-products";
 
 };
@@ -50,28 +50,17 @@ return(
 
 <div style={{padding:"20px"}}>
 
-<h2>Upload Product Image</h2>
+<h2>Upload Image</h2>
 
 <input type="file" onChange={handleFile} />
 
 <br/><br/>
 
 {preview && (
-
-<div>
-
-<p>Preview:</p>
-
-<img
-src={preview}
-style={{width:"200px",borderRadius:"10px"}}
-/>
-
-</div>
-
+<img src={preview} style={{width:"200px"}} />
 )}
 
-<br/>
+<br/><br/>
 
 <button onClick={upload}>
 Upload Image
