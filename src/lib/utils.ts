a178f5480ddeff0ae19034bd502e3f9868d1f495
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-18 15:52:52 +0530

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export function getTextColor(bg: string) {
+  const color = bg.replace("#", "");
+  const r = parseInt(color.substring(0,2),16);
+  const g = parseInt(color.substring(2,4),16);
+  const b = parseInt(color.substring(4,6),16);
+
+  const brightness = (r*299 + g*587 + b*114) / 1000;
+
+  return brightness > 150 ? "#000000" : "#ffffff";
+}
