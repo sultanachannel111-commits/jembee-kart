3fb72d6dd5a1604cde0b8ae92b0f499897b28704
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-22 17:45:46 +0530

    Update FestivalBanner.tsx

--- src/components/home/FestivalBanner.tsx
+++ src/components/home/FestivalBanner.tsx
@@ -9,25 +9,25 @@ export default function FestivalBanner({
 
   return(
 
-    <div className="rounded-xl overflow-hidden mx-3 mt-3 shadow-sm">
+    <div className="w-full overflow-hidden mt-3 shadow-sm">
 
       {/* IMAGE */}
       {festival.image && (
         <img
           src={festival.image}
-          className="w-full h-[100px] object-cover rounded-xl"
+          className="w-full h-[100px] object-cover"
         />
       )}
 
       {/* TITLE + TIMER */}
-      <div className="bg-gradient-to-r from-pink-500 to-purple-600 text-white px-3 py-1.5 -mt-2 rounded-b-xl">
+      <div className="bg-gradient-to-r from-pink-500 to-purple-600 text-white px-4 py-2">
 
         <h2 className="font-semibold text-sm">
           {festival.title}
         </h2>
 
         {timeLeft && (
-          <div className="flex gap-2 mt-1 text-[11px] font-semibold opacity-90">
+          <div className="flex gap-2 mt-1 text-[11px] font-semibold">
             <span>{timeLeft.hours}h</span>
             <span>{timeLeft.minutes}m</span>
             <span>{timeLeft.seconds}s</span>
