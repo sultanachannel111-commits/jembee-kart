40575933b2ef072e000fcace10a79a724b9169af
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-28 18:24:22 +0530

    Refactor getFinalPrice to include selectedSize price

--- src/utils/getFinalPrice.ts
+++ src/utils/getFinalPrice.ts
@@ -1,13 +1,13 @@
 export const getFinalPrice = (item: any) => {
   return Number(
-    item?.price ??
     item?.sellPrice ??
     item?.finalPrice ??
+    item?.selectedSize?.sellPrice ??
+    item?.variations?.[0]?.sizes?.[0]?.sellPrice ??
+    item?.price ??
     item?.originalPrice ??
     item?.selectedSize?.price ??
-    item?.selectedSize?.sellPrice ??
     item?.variations?.[0]?.sizes?.[0]?.price ??
-    item?.variations?.[0]?.sizes?.[0]?.sellPrice ??
     0
   );
 };
