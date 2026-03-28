e1d6c05311f3fbde7724d0a7d95daa9525e32f07
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-03-28 19:03:38 +0530

    Update getFinalPrice.ts

--- src/utils/getFinalPrice.ts
+++ src/utils/getFinalPrice.ts
@@ -1,13 +1,12 @@
 export const getFinalPrice = (item: any) => {
   return Number(
-    item?.sellPrice ??
-    item?.finalPrice ??
     item?.selectedSize?.sellPrice ??
     item?.variations?.[0]?.sizes?.[0]?.sellPrice ??
-    item?.price ??
-    item?.originalPrice ??
+    item?.sellPrice ??
+    item?.finalPrice ??
     item?.selectedSize?.price ??
     item?.variations?.[0]?.sizes?.[0]?.price ??
+    item?.price ??
     0
   );
 };
