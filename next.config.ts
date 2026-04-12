501b2a9a1aae4293dfe3ebeb1ffcccf62ec5bdae
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-04-12 05:45:30 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* APK build ke liye zaroori settings */
+  output: 'export', 
+  
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -9,6 +11,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true, // Static export mein images ke liye ye zaroori hai
     remotePatterns: [
       {
         protocol: 'https',
