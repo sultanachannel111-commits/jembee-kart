f5f71b0f8fe3956705312395ffe7dcc68a7be181
sultanachannel111-commits <sultanachannel111@gmail.com>
2026-04-12 15:17:01 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,29 +3,16 @@ import type { NextConfig } from "next";
 const isExport = process.env.GITHUB_ACTIONS === "true";
 
 const nextConfig: NextConfig = {
-  // ✅ APK build ke time static export
   output: isExport ? "export" : undefined,
-
-  // ✅ Static hosting ke liye required
   trailingSlash: true,
-
-  // ✅ Image issues fix (important for export)
   images: {
     unoptimized: true,
   },
-
-  // ✅ Build fail na ho (APK ke liye safe)
   typescript: {
     ignoreBuildErrors: true,
   },
-
   eslint: {
     ignoreDuringBuilds: true,
-  },
-
-  // ✅ Optional (extra safe for static export)
-  experimental: {
-    appDir: true,
   },
 };
 
